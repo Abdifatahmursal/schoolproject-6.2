36831d679d02a43267ba8d057fedce07caa9c525
Abdifatahmursal <113897628+Abdifatahmursal@users.noreply.github.com>
2022-12-06 12:24:05 +0000

    Add files via upload

---
+++ opdracht4.2.php
@@ -0,0 +1,14 @@
+<?php
+$laptop = 1000;
+$software = 150;
+
+if($laptop > 150) {
+    $som = $laptop * 1.19 ; 
+    echo "De oude prijs is:  $laptop";
+}
+
+if($laptop <55){
+ $som = $laptop * 1.11;
+ echo "De oude prijs is: $euro; $laptop,";
+}
+?>
---
+++ opdracht4.4.php
@@ -0,0 +1,13 @@
+<?php
+ 
+ $dag = ("23");
+ switch($dag){
+ case $dag < 6 : echo "het is nacht"; break;
+ case $dag >6 && $dag <12 : echo "het is ochtend"; break;
+ case $dag >12 && $dag <18 : echo "het is middag"; break;
+ case $dag >18 && $dag <24 : echo "het is avond"; break;
+
+ }
+
+
+?>
